b137dfd7d003ec75a18f28859114ae1cb4d3656c
Adrian Oprea <adrian@codesi.nz>
2017-01-27 22:23:08 +0200

    fix: Hide notification button when form was submitted

--- themes/zodkoo-dark/static/js/app.js
+++ themes/zodkoo-dark/static/js/app.js
@@ -3,25 +3,31 @@
   var $footerForm = $('#footer-FORM');
   var isHeroFormSubmitted = localStorage.getItem('heroForm#submitted') === 'true';
   var isFooterFormSubmitted = localStorage.getItem('footerForm#submitted') === 'true';
+  var notifyMe = $('.navbar-right');
+  var HIDE_ANIMATION_DURATION = 300;
 
   if (isHeroFormSubmitted) {
     $heroForm.hide();
+    notifyMe.hide();
   } else {
     $heroForm.on('submit', function(event) {
       $.post($heroForm.attr('action'), $heroForm.serialize());
       localStorage.setItem('heroForm#submitted', true);
-      $heroForm.hide();
+      $heroForm.hide(HIDE_ANIMATION_DURATION);
+      notifyMe.hide(HIDE_ANIMATION_DURATION);
       event.preventDefault();
     });
   }
 
   if (isFooterFormSubmitted) {
     $('#contact').hide();
+    notifyMe.hide();
   } else {
     $footerForm.on('submit', function(event) {
       $.post($footerForm.attr('action'), $footerForm.serialize());
       localStorage.setItem('footerForm#submitted', true);
-      $('#contact').hide();
+      $('#contact').hide(HIDE_ANIMATION_DURATION);
+      notifyMe.hide(HIDE_ANIMATION_DURATION);
       event.preventDefault();
     });
   }
